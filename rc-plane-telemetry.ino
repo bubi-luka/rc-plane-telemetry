9f02c96dec8afb6d6884cceb2cd26c8961464db9
Luka Oman <bubi-luka@users.noreply.github.com>
2023-09-25 10:20:00 +0200

    FEATURE: Add boilerplate INO file
    Add empty Arduino IDE sketch file

---
+++ rc-plane-telemetry.ino
@@ -0,0 +1,26 @@
+/* *************************************************************************** *
+ * Project Title: 
+ * 
+ * Description: 
+ *
+ * Licence: GPL 3.0
+ * Version: 0.0.0
+ * created: DD. MM. YYYY
+ * by:      FirstName LastName
+ * *************************************************************************** */
+
+// Declare additional libraries
+// Define Arduino pins
+// Define new objects
+// Define variables
+// Define custom functions
+
+// Initialization function, run only once
+void setup() {
+
+}
+
+// Main function, run in a loop
+void loop() {
+
+}
