d4646199a912402c09515362070015921d5bbed6
Luka Oman <bubi-luka@users.noreply.github.com>
2023-09-25 11:00:12 +0200

    FEATURE: Add iBUS communication with the RC receiver
    We use iBusBM library to connect Arduino with RC receiver and
    send dummy data to the RC transmitter on the ground.

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
 # RC Plane Telemetry
-**A small Arduino project that uses various sensors to gather data and sends it back to the user radio control unit.**
+**An Arduino project that uses various sensors to gather data and sends it back to the user radio control unit.**
 
 ## Description
 
-## Installation
+### Materials
+- Arduino Nano board
+- radio receiver TGY-IA6B or similar
+- wires
+- diode 1N4148
 
-## Usage
+### Used libraries
+- [iBusBM](https://github.com/bmellink/IBusBM/)
+**!! Thank you all for your work !!**
+
+## Installation and Usage
+1. Clone the repository
+2. Connect sensors to the Arduino
+3. Compile and upload the code to the Arduino
+4. Connect the receiver wires for the iBUS (only after uploading the code)
+5. Wait for all the sensors start and GPS connects
+6. Fly and enjoy
+
+## Custumizations
+It is possible to define up to 10 sensors. The available codes are:
+* iBusSensor.addSensor(0x00); // Internal Voltage [V]   ()
+* iBusSensor.addSensor(0x01); // Temperature      [°C]  (400 + temp * 10)
+* iBusSensor.addSensor(0x02); // Motor rotations  [RPM] ()
+* iBusSensor.addSensor(0x03); // External Voltage [V]   ()
+* iBusSensor.addSensor(0x41); // Pressure         [hPa] ()
+* iBusSensor.addSensor(0xfd); // Servo            [?]   ()
 
 ## Roadmap
-### 0.0.1
-- 
+### 0.1.0
+- add iBUS communication path to send data from Arduino to RC transmitter module
+
+### 0.2.0
+- add external battery voltage sensor
+
+### 0.3.0
+- add altitude sensor
+
+### 0.4.0
+- add gyroscope sensor
+
+### 0.5.0
+- add compass sensor
+
+### 0.6.0
+- add SD card reader/writer module to log data
+
+### 0.7.0
+- add GPS sensor
 
 ## Change Log
 ### 0.0.1
--- rc-plane-telemetry.ino
+++ rc-plane-telemetry.ino
@@ -5,23 +5,52 @@
  *              data and sends it back to the user radio control unit.
  *
  * Licence: GPL 3.0       (details in project file "LICENCE")
- * Version: 0.0.1         (details in project file "README.md")
+ * Version: 0.1.0         (details in project file "README.md")
  * created: 25. 09. 2023
  * by:      Luka Oman
  * *************************************************************************** */
 
 // Declare additional libraries
+#include <IBusBM.h>  // Library for iBus telemetry
+
 // Define Arduino pins
+
 // Define new objects
+IBusBM iBusSensor;  // iBus telecomunication for sensors
+
 // Define variables
+
 // Define custom functions
 
 // Initialization function, run only once
 void setup() {
 
+  // start iBus comunication
+  //*****************************************************************************
+  iBusSensor.begin(Serial);  // iBusSensor.begin(Serial, IBUSBM_NOTIMER);
+
+  iBusSensor.addSensor(0x01);  // Temperature				[°C]  (400 + temp * 10)
+  iBusSensor.addSensor(0x03);  // External Voltage	[V]   ()
+  iBusSensor.addSensor(0x41);  // Pressure					[hPa] ()
+  iBusSensor.addSensor(0xfd);  // Relative altitude	[m]   ()
+  iBusSensor.addSensor(0xfd);  // Azimuth						[°]   ()
+  iBusSensor.addSensor(0xfd);  // Speed							[m/s] ()
+  //*****************************************************************************
 }
 
 // Main function, run in a loop
 void loop() {
 
+  // Send gathered data to the remote control unit on the ground
+  //*****************************************************************************
+  iBusSensor.setSensorMeasurement(1, 655);     // Temperature				[°C]  (400 + temp * 10)
+  iBusSensor.setSensorMeasurement(2, 753);     // External Voltage	[V]   ()
+  iBusSensor.setSensorMeasurement(3, 951);  // Pressure					[hPa] ()
+  iBusSensor.setSensorMeasurement(4, 47);   // Relative altitude	[m]   ()
+  iBusSensor.setSensorMeasurement(5, 0);       // Azimuth						[°]   ()
+  iBusSensor.setSensorMeasurement(6, 17);    // Speed							[m/s] ()
+
+  // send data to iBus
+  iBusSensor.loop();
+  //*****************************************************************************
 }
